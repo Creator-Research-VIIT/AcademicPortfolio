aa52a8da41a1199100c3279aeb347604012f5fa8
VIITxCreator <creatorresearchxviit@gmail.com>
2025-09-19 15:13:36 +0530

    Update goatcounter.jsx

--- components/goatcounter.jsx
+++ components/goatcounter.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect } from "react";
 import Script from "next/script";
 import { usePathname, useSearchParams } from "next/navigation";
@@ -7,16 +8,20 @@ export default function GoatCounterClient() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  // Trigger count on route changes
   useEffect(() => {
-    if (typeof window !== "undefined" && window.goatcounter && window.goatcounter.count) {
+    if (typeof window !== "undefined" && window.goatcounter?.count) {
       const qs = searchParams?.toString() ? `?${searchParams.toString()}` : "";
       window.goatcounter.count({ path: pathname + qs });
     }
   }, [pathname, searchParams]);
 
   return (
-    <Script data-goatcounter="https://yashrathod.goatcounter.com/count"
-        async src="//gc.zgo.at/count.js"></Script>
-  );
+    <Script
+      src="//gc.zgo.at/count.js"
+      data-goatcounter="https://yashrathod.goatcounter.com/count"
+      data-goatcounter-settings='{"allow_local": true}'
+      strategy="afterInteractive"
+      async
+    />
+  );
 }
