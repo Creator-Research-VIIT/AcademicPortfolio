b1ff091ff9ace2d4d58fc416d4213792197c470c
VIITxCreator <creatorresearchxviit@gmail.com>
2025-09-19 15:10:22 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,9 @@
 import { Inter } from 'next/font/google';
 import '../public/styles/globals.css';
 import LayoutWrapper from '../components/LayoutWrapper';
-
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 import GoatCounterClient from '@/components/goatcounter';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,9 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-background text-text`}>
+          <Suspense fallback={null}>
           <GoatCounterClient />
+        </Suspense>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
-    </html>
-  );
+    </html>
+  );
 }
